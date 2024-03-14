d70233741b47b3c2bd07efaf95c3e0e8329865c3
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-03-14 10:35:31 +0700

    add gitignore

--- C file/pipeline_03.c
+++ C file/pipeline_03.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-	int i,j;
-	long sum,x;
-	
-	for (i=0;i<10;i++){
-		for(j=0;j<10;j++){
-			x=i+j;
-			sum = sum+x;
+	int i, j;
+	long sum, x;
+	sum = 0;
+	x = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			x = i + j;
+			sum = sum + x;
+			printf("x=%d sum = %d\n", x, sum);
 		}
+		printf("i:%d j:%d x:%d sum:%d\n", i, j, x, sum);
 	}
-	printf("%ld\n",sum);
+	printf("%ld\n", sum);
 }
